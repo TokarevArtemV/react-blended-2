c90a135729362db47449161e5fba8763bfdd9250
klodmone <kladmone54@gmail.com>
2023-12-23 11:56:44 +0100

    Update Gallery.jsx

--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -8,34 +8,68 @@ export class Gallery extends Component {
     query: '',
     page: 1,
     photos: [],
+    isMorePhotos: false,
+    randomId: 0,
+    error: null,
+    isEmpty: false,
   };
 
   async componentDidUpdate(_, prevState) {
-    const { query, page } = this.state;
-    if (prevState.query !== query || prevState.page !== page) {
-      const { photos, total_results } = await ImageService.getImages(
-        query,
-        page
-      );
-      this.setState(prevState => ({
-        photos: [...prevState.photos, ...photos],
-      }));
+    const { query, page, randomId } = this.state;
+    if (
+      prevState.query !== query ||
+      prevState.page !== page ||
+      prevState.randomId !== randomId
+    ) {
+      try {
+        const { photos, total_results } = await ImageService.getImages(
+          query,
+          page
+        );
+        if (photos.length === 0) {
+          this.setState({ isEmpty: true });
+          return;
+        }
+        this.setState(prevState => ({
+          photos: [...prevState.photos, ...photos],
+          isLoadMore: page < Math.ceil(total_results / 15),
+        }));
+      } catch (error) {
+        this.setState({ error: error.message });
+      }
     }
   }
 
   onSubmit = query => {
     this.setState({
       query,
+      page: 1,
+      photos: [],
+      isLoadMore: false,
+      randomId: Math.random(),
+      isEmpty: false,
     });
   };
 
+  handleLoadMore = () => {
+    this.setState(prevState => ({ page: prevState.page + 1 }));
+  };
+
   render() {
-    const { photos } = this.state;
+    const { photos, isLoadMore, error, isEmpty } = this.state;
     return (
       <>
         <SearchForm onSubmit={this.onSubmit} />
         {photos.length > 0 && <GalleryList photos={photos} />}
-        {/* <Text textAlign="center">Sorry. There are no images ... 😭</Text> */}
+        {isLoadMore && (
+          <Button type="button" onClick={this.handleLoadMore}>
+            Load more
+          </Button>
+        )}
+        {error && <Text textAlign="center">Sorry. {error} 😭</Text>}
+        {isEmpty && (
+          <Text textAlign="center">Sorry. There are no images... 😭</Text>
+        )}
       </>
     );
   }
